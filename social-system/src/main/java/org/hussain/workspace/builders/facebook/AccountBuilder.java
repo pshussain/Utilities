213142cf2579547e2dfca037bea4632bdfb36f55
pshussain <pshussain@gmail.com>
2014-07-01 15:50:23 +0530

    Added more methods in account builder.

--- social-system/src/main/java/org/hussain/workspace/builders/facebook/AccountBuilder.java
+++ social-system/src/main/java/org/hussain/workspace/builders/facebook/AccountBuilder.java
@@ -1,9 +1,26 @@
 package org.hussain.workspace.builders.facebook;
 
 public interface AccountBuilder {
-	public void addAccount(Integer a);
+	public void createAccount(String name, String currency, Integer timezoneId);
 
-	public void addAccount(String a);
+	public void updateCreditLine(String accountId, String accountPayableEmail);
 
-	public void addAccount(Short a);
+	public void updateSpendCap(String accountId, Integer spendCap);
+
+	public void updateSpendCapAction(String accountId, String action);
+
+	public void updateAccountName(String accountId, String name);
+
+	public void updateAgencyClientDeclaration(String accountId, Object name);
+
+	public void updateEndAdvertiser(String accountId, Long pageOrAppId);
+
+	public void updateMediaAgency(String accountId, Long pageOrAppId);
+
+	public void updatePartner(String accountId, Long pageOrAppId);
+
+	public void read(String accountId);
+
+	public void read(String accountId, String fields);
+
 }
--- social-system/src/main/java/org/hussain/workspace/builders/facebook/impl/FacebookAdaccountBuilder.java
+++ social-system/src/main/java/org/hussain/workspace/builders/facebook/impl/FacebookAdaccountBuilder.java
@@ -3,7 +3,12 @@ package org.hussain.workspace.builders.facebook.impl;
 import org.hussain.workspace.builders.facebook.AccountBuilder;
 import org.hussain.workspace.crud.FacebookCRUD;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 public class FacebookAdaccountBuilder implements FacebookCRUD, AccountBuilder {
+	private String url = "me/adaccounts";
+	JsonArray accountArray;
 
 	public String create() {
 		// TODO Auto-generated method stub
@@ -25,17 +30,62 @@ public class FacebookAdaccountBuilder implements FacebookCRUD, AccountBuilder {
 
 	}
 
-	public void addAccount(Integer a) {
+	public void createAccount(String name, String currency, Integer timezoneId) {
+		JsonObject account = new JsonObject();
+		account.addProperty("method", "POST");
+		account.addProperty("relative_url", url);
+		account.addProperty("body", "name=" + name + "&currency=" + currency
+				+ "&timezone_id=" + timezoneId);
+		accountArray.add(account);
+
+	}
+
+	public void updateCreditLine(String accountId, String accountPayableEmail) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void addAccount(String a) {
+	public void updateSpendCap(String accountId, Integer spendCap) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void addAccount(Short a) {
+	public void updateSpendCapAction(String accountId, String action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateAccountName(String accountId, String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateAgencyClientDeclaration(String accountId, Object name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateEndAdvertiser(String accountId, Long pageOrAppId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateMediaAgency(String accountId, Long pageOrAppId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updatePartner(String accountId, Long pageOrAppId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void read(String accountId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void read(String accountId, String fields) {
 		// TODO Auto-generated method stub
 
 	}
